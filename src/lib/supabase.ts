769bc942ba418784afcba8e42cfd34f9e15c8083
Keerthi Sree Marrapu <mksree@MacBook-Air.local>
2025-07-27 10:26:12 +0530

    troibleshoot supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 // src/lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zzpioibyqqypxebzmocw.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6cGlvaWJ5cXF5cHhlYnptb2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1ODk1MzksImV4cCI6MjA2OTE2NTUzOX0.wG8iqzqfMwsVRD1RAOoc7fHVTOt8WmnprFVnmnwrZZ4';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
